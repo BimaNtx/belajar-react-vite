c94a190027addc87c86828684893a712b8682734
Bima Ananta <binarybeast029@gmail.com>
2026-07-01 04:00:49 +0700

    Menambah tombol suka dan raset pada biodata

--- src/components/Biodata.jsx
+++ src/components/Biodata.jsx
@@ -1,11 +1,26 @@
-const Biodata = ({ nama, jurusan, hobi }) => {
+import { useState } from "react";
+
+const Biodata = ({nama, jurusan, hobi}) => {
+  const [jumlahLike, setJumlahLike] = useState(0);
+
   return (
-    <div style={{ border: "1px solid white", padding: "10px", margin: "10px", borderRadius: "8px" }}>
-    <h2>Nama: {nama}</h2>
-    <p>Jurusan: {jurusan}</p>
-    <p>Fokus: {hobi}</p>
+    <div style={{ border: "1px solid white", padding: "10px", margin: "10px", borderRadius: "10px"}}>
+      <h2>Nama: {nama}</h2>
+      <p>Jurusan: {jurusan}</p>
+      <p>Hobi: {hobi}</p>
+
+      <button onClick={() => setJumlahLike(jumlahLike + 1)}
+        style={{cursor: "pointer", padding: "5px 10px", marginTop: "10px" }}
+        >
+          Sukai Profil ({jumlahLike})
+        </button>
+
+        <button onClick={() => setJumlahLike(0)}
+          style={{cursor: "pointer", padding: "5px 10px", marginBottom: "10px"}}>
+            Reset
+          </button>
     </div>
   );
 };
 
-export default Biodata
+export default Biodata;
